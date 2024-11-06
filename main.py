4bef000e6753057dbfe431c663e68eec42f1f857
SebastienTs <sebastien.tosi@gmail.com>
2024-11-06 14:36:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+# 3D cells segmentation example with two channels (membrane only and nuclei)
+
+import napari
+from os import chmod
+from napari import Viewer
+from magicgui import magicgui
+from tifffile import imread, imwrite
+from skimage.filters import gaussian
+from skimage.feature import blob_dog as dog
+from skimage.segmentation import watershed
+from skimage.measure import regionprops_table
+from scipy.ndimage import zoom, minimum_filter, median_filter
+from utils import *
+import pandas as pd
+
+# Image file from Airy scan (0.25 XY downscaled)
+imagefile_default = 'D:/Projects/UPF/Berta_Lucas/CAAXinjH2B 12 hpf_025_crop.tif'
+
+@magicgui(call_button='Run',
+          imagefile={'widget_type': 'FileEdit', 'label': 'Image Stack'},
+          nuc_prescale={'widget_type': 'FloatSlider', 'min': 0, 'max': 1},
+          nuc_scale_min={'widget_type': 'IntSlider', 'min': 1, 'max': 9},
+          nuc_scale_max={'widget_type': 'IntSlider', 'min': 1, 'max': 9},
+          nuc_det={'widget_type': 'FloatSlider', 'min': 0, 'max': 1e-1},
+          memb_gaussrad={'widget_type': 'FloatSlider', 'min': 0, 'max': 1.5},
+          memb_maxdelta={'widget_type': 'IntSlider', 'min': 1, 'max': 100},
+          cell_maxdst={'widget_type': 'IntSlider', 'min': 1, 'max': 100},
+          cell_regrad={'widget_type': 'IntSlider', 'min': 1, 'max': 9},
+          cell_minvol={'widget_type': 'IntSlider', 'min': 1, 'max': 1e3},
+          cell_maxvol={'widget_type': 'IntSlider', 'min': 1, 'max': 2e5},
+          zratio={'widget_type': 'FloatSlider', 'min': 0, 'max': 9})
+def tissueseg3d(vw: Viewer, imagefile=imagefile_default, nuc_prescale=0.5, nuc_scale_min = 2, nuc_scale_max = 6,
+                nuc_det = 5e-2, memb_gaussrad=0.5, memb_maxdelta=25, cell_maxdst=50, cell_regrad=5, cell_minvol=5e2,
+                cell_maxvol=1e5, zratio = 3):
+
+    # Close all layers
+    vw.layers.clear()
+
+    # Load image
+    print('Loading image stack and preprocessing...')
+    stack = imread(imagefile)
+
+    # Split channels
+    nuclei = stack[:, 0, :, :]
+    membrane = stack[:, 1, :, :]
+    membrane_flt = gaussian(membrane.astype(float), sigma=memb_gaussrad, preserve_range=True).astype('uint16')
+
+    #### Detect nuclei
+    # Multiscale DoG
+    print('Performing nuclei detection...')
+    blobs = dog(zoom(nuclei, (1, nuc_prescale, nuc_prescale), order=1), min_sigma=nuc_scale_min,
+                max_sigma=nuc_scale_max, sigma_ratio=1.6, threshold=nuc_det*1e-3, exclude_border=False)
+    coords = [(int(blob[0]), int(blob[1]/nuc_prescale), int(blob[2]/nuc_prescale)) for blob in blobs]
+    # Remove spurious seeds
+    #nuclei_msk = (nuclei>=nuc_thr).astype(int)
+    coords_kept = remove_seeds(membrane_flt, coords, cell_maxdst, memb_maxdelta, zratio)
+
+    #### Segment cells
+    # Watershed
+    print('Performing cells segmentation...')
+    seeds = np.zeros(nuclei.shape, dtype=np.uint16)
+    for i, sd in enumerate(coords_kept):
+        seeds[sd] = i+1
+    membrane_flt = imposemin(membrane_flt, seeds>0)
+    cell_lbl = watershed(membrane_flt, seeds, compactness=0)
+    # Remove small and large regions
+    print('Postprocessing...')
+    cell_lbl = remove_components(cell_lbl, cell_minvol, cell_maxvol)
+    # Regularize, fill holes (and optionally shrink) label mask
+    cell_lbl = median_filter(cell_lbl, size=(np.ceil(cell_regrad/zratio).astype(int),cell_regrad,cell_regrad))
+    cell_lbl = fill_lbl_holes(cell_lbl)
+    # Split cell regions
+    #cell_lbl = cell_lbl * (cell_lbl == minimum_filter(cell_lbl, size=(1,3,3)))
+
+    #### Export results
+    print('Exporting results...')
+    imwrite(str(imagefile).replace('.tif', '_lbl.tif'), cell_lbl)
+    chmod(str(imagefile).replace('.tif', '_lbl.tif'), 0o666)
+    properties = regionprops_table(cell_lbl, properties=['label', 'centroid', 'area', 'MajorAxisLength', 'MinorAxisLength'])
+    df = pd.DataFrame(properties)
+    df.to_csv(str(imagefile).replace('.tif', '_lbl.csv'), index=False)
+    chmod(str(imagefile).replace('.tif', '_lbl.csv'), 0o666)
+    print(f'Number of segmented cells: {len(df)}')
+
+    #### Display results
+    print('Displaying results...')
+    np.random.seed(0)
+    viewer.add_image(nuclei, name=f"Nuclei", scale=(zratio, 1, 1))
+    viewer.add_image(membrane, name=f"Membrane", scale=(zratio, 1, 1), blending='additive', colormap='green')
+    viewer.add_labels(cell_lbl, name=f"CellsLbl", blending="additive", scale=(zratio, 1, 1))
+    viewer.add_points(coords, name=f"Seeds", size=15, face_color='black', blending="additive", scale=(zratio, 1, 1), visible=False)
+    viewer.add_points(coords_kept, name=f"Seeds_Kept", size=15, face_color='green', blending="additive", scale=(zratio, 1, 1), visible=False)
+
+viewer = napari.Viewer()
+viewer.window.add_dock_widget(tissueseg3d, area='right', name='Process')
+napari.run()
---
+++ utils.py
@@ -0,0 +1,60 @@
+import numpy as np
+from skimage.morphology import reconstruction
+from skimage.measure import label, regionprops
+from scipy.ndimage import binary_fill_holes, maximum_filter
+from qtpy.QtWidgets import QTableView
+from qtpy.QtCore import QAbstractTableModel, Qt
+
+def distance(pt1, pt2, zratio):
+    dst = np.sqrt(((np.array(pt1)*np.array((zratio,1,1))-np.array(pt2)*np.array((zratio,1,1)))**2).sum())
+    return dst
+
+def interpolate_3d_line(start, end):
+    start = np.array(start)
+    end = np.array(end)
+    vector = end - start
+    num_steps = int(np.ceil(np.linalg.norm(vector)))
+    t = np.linspace(0, 1, num_steps)
+    points = start[np.newaxis, :] + t[:, np.newaxis] * vector[np.newaxis, :]
+    return np.round(points).astype(int)
+
+def remove_seeds(img, seeds, dstthr, deltamax, zratio):
+    mergelst = [[] for _ in range(len(seeds))]
+    for i, seed1 in enumerate(seeds):
+        for j, seed2 in enumerate(seeds[i+1:], start=i+1):
+                if  distance(seed1, seed2, zratio) < dstthr:
+                    profile = np.array([img[tuple(point)] for point in interpolate_3d_line(seed1, seed2)])
+                    delta = profile.max() - profile.min()
+                    if delta < deltamax:
+                        mergelst[i].append(j)
+
+    # Seeds to be kept (all but the ones that are part of a cluster)
+    idx = set(range(1, len(seeds))) - set(sum(mergelst, []))
+
+    # Recenter seeds at clusters' centers of mass
+    seeds = [tuple(np.round(np.mean(np.array(seeds)[[i]+lst], axis=0)).astype(int)) for i, lst in enumerate(mergelst)]
+
+    return [seeds[i] for i in idx]
+
+# Remove small and large connected components in label mask
+def remove_components(label_mask, minvol, maxvol):
+    labels, counts = np.unique(label_mask, return_counts=True)
+    mask = (counts >= minvol) & (counts <= maxvol)
+    mapping = np.zeros(labels.max() + 1, dtype=label_mask.dtype)
+    mapping[labels[mask]] = labels[mask]
+    return mapping[label_mask]
+
+def imposemin(img, minima):
+    marker = np.full(img.shape, np.inf)
+    marker[minima == 1] = 0
+    mask = np.minimum((img + 1), marker)
+    return reconstruction(marker, mask, method='erosion')
+
+def fill_lbl_holes(lbl):
+    lbl_holes = label(binary_fill_holes(lbl>0) ^ (lbl>0))
+    regions = regionprops(lbl_holes, intensity_image=maximum_filter(lbl, size=(3,3,3)))
+    for region in regions:
+        reglbl = region.intensity_max
+        x, y, z = zip(*region.coords)
+        lbl[x, y, z] = reglbl
+    return lbl
