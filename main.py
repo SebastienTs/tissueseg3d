c6bbb94fe9470e2a015e1b6053bd6a28df955903
SebastienTs <sebastien.tosi@gmail.com>
2024-11-14 12:15:11 +0100

    Added independent image loader + support for object surfaces, further small improvements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # 3D cells segmentation example with two channels (membrane only and nuclei)
 
 import napari
+import pandas as pd
 from os import chmod
 from napari import Viewer
 from magicgui import magicgui
@@ -11,49 +12,39 @@ from skimage.segmentation import watershed
 from skimage.measure import regionprops_table
 from scipy.ndimage import zoom, minimum_filter, median_filter
 from utils import *
-import pandas as pd
 
-# Image file from Airy scan (0.25 XY downscaled)
-imagefile_default = 'D:/Projects/UPF/Berta_Lucas/CAAXinjH2B 12 hpf_025_crop.tif'
-
-@magicgui(call_button='Run',
-          imagefile={'widget_type': 'FileEdit', 'label': 'Image Stack'},
+@magicgui(call_button='Process',
           nuc_prescale={'widget_type': 'FloatSlider', 'min': 0, 'max': 1},
           nuc_scale_min={'widget_type': 'FloatSlider', 'min': 1, 'max': 5},
           nuc_scale_max={'widget_type': 'FloatSlider', 'min': 1, 'max': 9},
-          nuc_det={'widget_type': 'FloatSlider', 'min': 0, 'max': 1e-1},
-          memb_gaussrad={'widget_type': 'FloatSlider', 'min': 0, 'max': 1.5},
+          nuc_det_thr={'widget_type': 'FloatSlider', 'min': 0, 'max': 1e-1},
+          nuc_maxdst={'widget_type': 'IntSlider', 'min': 1, 'max': 100},
           memb_maxdelta={'widget_type': 'IntSlider', 'min': 1, 'max': 100},
-          cell_maxdst={'widget_type': 'IntSlider', 'min': 1, 'max': 100},
+          memb_gaussrad={'widget_type': 'FloatSlider', 'min': 0, 'max': 1.5},
           cell_regrad={'widget_type': 'IntSlider', 'min': 1, 'max': 9},
-          cell_minvol={'widget_type': 'IntSlider', 'min': 1, 'max': 1e3},
-          cell_maxvol={'widget_type': 'IntSlider', 'min': 1, 'max': 2e5},
-          zratio={'widget_type': 'FloatSlider', 'min': 0, 'max': 9})
-def tissueseg3d(vw: Viewer, imagefile=imagefile_default, nuc_prescale=0.5, nuc_scale_min = 2, nuc_scale_max = 6,
-                nuc_det = 5e-2, memb_gaussrad=0.5, memb_maxdelta=25, cell_maxdst=50, cell_regrad=5, cell_minvol=5e2,
-                cell_maxvol=1e5, zratio = 6):
+          cell_minvol={'widget_type': 'IntSlider', 'min': 1, 'max': 5e3},
+          cell_maxvol={'widget_type': 'IntSlider', 'min': 1, 'max': 2e5})
+def tissueseg3d(vw: Viewer, nuc_prescale=0.5, nuc_scale_min = 2, nuc_scale_max = 6, nuc_det_thr = 5e-2, 
+                nuc_maxdst=50, memb_maxdelta=25, memb_gaussrad=0.5, cell_regrad=5, cell_minvol=2e3, cell_maxvol=1e5):
 
-    # Close all layers
-    vw.layers.clear()
+    # Retrieve data
+    imagefile = load_image_tiff.imagefile.value
+    zratio = load_image_tiff.zratio.value
+    nuclei = vw.layers['Nuclei'].data
+    membrane = vw.layers['Membrane'].data
 
-    # Load image
-    print('Loading image stack and preprocessing...')
-    stack = imread(imagefile)
-
-    # Split channels
-    nuclei = stack[:, 0, :, :]
-    membrane = stack[:, 1, :, :]
+    # Filter membrane channel
     membrane_flt = gaussian(membrane.astype(float), sigma=memb_gaussrad, preserve_range=True).astype('uint16')
 
     #### Detect nuclei
     # Multiscale DoG
     print('Performing nuclei detection...')
     blobs = dog(zoom(nuclei, (1, nuc_prescale, nuc_prescale), order=1), min_sigma=nuc_scale_min,
-                max_sigma=nuc_scale_max, sigma_ratio=1.6, threshold=nuc_det*1e-3, exclude_border=False)
+                max_sigma=nuc_scale_max, sigma_ratio=1.6, threshold=nuc_det_thr*1e-3, exclude_border=False)
     coords = [(int(blob[0]), int(blob[1]/nuc_prescale), int(blob[2]/nuc_prescale)) for blob in blobs]
     # Remove spurious seeds
     #nuclei_msk = (nuclei>=nuc_thr).astype(int)
-    coords_kept = remove_seeds(membrane, coords, cell_maxdst, memb_maxdelta, zratio)
+    coords_kept = remove_seeds(membrane, coords, nuc_maxdst, memb_maxdelta, zratio)
 
     #### Segment cells
     # Watershed
@@ -74,29 +65,35 @@ def tissueseg3d(vw: Viewer, imagefile=imagefile_default, nuc_prescale=0.5, nuc_s
     cell_lbl = remove_components_size(cell_lbl, cell_minvol, cell_maxvol)
     cell_lbl = remove_components_edge(cell_lbl)
     cell_lbl = fill_lbl_holes(cell_lbl)
-    # Split cell regions
-    #cell_lbl = cell_lbl * (cell_lbl == minimum_filter(cell_lbl, size=(1,3,3)))
+    cell_lbl = relabel_consecutive(cell_lbl)
+    cell_lbl = cell_lbl * (cell_lbl == minimum_filter(cell_lbl, size=(1,3,3)))
+    properties = regionprops_table(cell_lbl, properties=['label', 'centroid', 'area', 'MajorAxisLength', 'MinorAxisLength'])
+    # Compute object surfaces from label mask
+    #combined_verts, combined_faces, combined_values, custom_colormap = lbl2mesh(cell_lbl)
 
     #### Export results
     print('Exporting results...')
-    imsave(str(imagefile).replace('.tif', '_lbl.tif'), cell_lbl)
+    imsave(str(imagefile).replace('.tif', '_lbl.tif'), cell_lbl, check_contrast=False)
+    print(str(imagefile).replace('.tif', '_lbl.tif'))
     chmod(str(imagefile).replace('.tif', '_lbl.tif'), 0o666)
-    properties = regionprops_table(cell_lbl, properties=['label', 'centroid', 'area', 'MajorAxisLength', 'MinorAxisLength'])
     df = pd.DataFrame(properties)
+    df.columns = ['Cell', 'CZ', 'CY', 'CX', 'Volume (vox)', 'MajorAxis (pix)', 'MinorAxis (pix)']
     df.to_csv(str(imagefile).replace('.tif', '_lbl.csv'), index=False)
+    print(str(imagefile).replace('.tif', '_lbl.csv'))
     chmod(str(imagefile).replace('.tif', '_lbl.csv'), 0o666)
     print(f'Number of segmented cells: {len(df)}')
 
     #### Display results
     print('Displaying results...')
     np.random.seed(0)
-    vw.add_image(nuclei, name=f"Nuclei", scale=(zratio, 1, 1))
-    vw.add_image(membrane, name=f"Membrane", scale=(zratio, 1, 1), blending='additive', colormap='green')
     vw.add_labels(cell_lbl, name=f"CellsLbl", blending="additive", scale=(zratio, 1, 1))
+    #viewer.add_surface((combined_verts, combined_faces, combined_values), name='Combined Surface', colormap=custom_colormap, scale=(zratio, 1, 1))
     vw.add_points(coords, name=f"Seeds", size=15, face_color='black', blending="additive", scale=(zratio, 1, 1), visible=False)
     vw.add_points(coords_kept, name=f"Seeds_Kept", size=15, face_color='green', blending="additive", scale=(zratio, 1, 1), visible=False)
 
 viewer = napari.Viewer()
-dw = viewer.window.add_dock_widget(tissueseg3d, area='right', name='Process')
-dw.setMinimumWidth(360)
+dw1 = viewer.window.add_dock_widget(load_image_tiff, area='right', name='Load')
+dw1.setMinimumHeight(120);dw1.setMaximumHeight(120)
+dw1.setMinimumWidth(360)
+dw2 = viewer.window.add_dock_widget(tissueseg3d, area='right', name='Process')
 napari.run()
--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 import numpy as np
 from skimage.morphology import reconstruction
-from skimage.measure import label, regionprops
+from skimage.measure import label, regionprops, marching_cubes
 from scipy.ndimage import binary_fill_holes, maximum_filter
+from napari import Viewer
+from skimage.io import imread
+from magicgui import magicgui
+from napari.utils.colormaps import Colormap
 from qtpy.QtWidgets import QTableView
 from qtpy.QtCore import QAbstractTableModel, Qt
+
+# Image file from Airy scan (0.25 XY downscaled)
+imagefile_default = 'D:/Projects/UPF/Berta_Lucas/CAAXinjH2B 12 hpf_025_crop.tif'
 
 def distance(pt1, pt2, zratio):
     dst = np.sqrt(((np.array(pt1)*np.array((zratio,1,1))-np.array(pt2)*np.array((zratio,1,1)))**2).sum())
@@ -66,3 +73,58 @@ def fill_lbl_holes(lbl):
         x, y, z = zip(*region.coords)
         lbl[x, y, z] = reglbl
     return lbl
+
+def relabel_consecutive(lbl):
+    # Get unique labels, excluding 0 if it's present
+    unique_labels = np.unique(lbl)
+    unique_labels = unique_labels[unique_labels != 0]
+
+    # Create a mapping from old labels to new labels
+    label_map = {old: new for new, old in enumerate(unique_labels, start=1)}
+
+    # Create a vectorized function to apply the mapping
+    vectorized_map = np.vectorize(lambda x: label_map.get(x, 0))
+
+    # Apply the mapping to the entire mask
+    return vectorized_map(lbl)
+
+
+def lbl2mesh(lbl):
+    unique_labels = np.unique(lbl)
+    unique_labels = unique_labels[unique_labels != 0]
+    num_labels = len(unique_labels)
+
+    all_verts = []
+    all_faces = []
+    all_values = []
+    face_offset = 0
+    colors = [np.random.random(3) for _ in range(num_labels)]
+    custom_colormap = Colormap(colors=colors, name='custom_colormap', controls=np.linspace(0, 1, num_labels))
+    for i, label in enumerate(range(1, num_labels)):
+        verts, faces, _, _ = marching_cubes(lbl == label)
+        all_verts.append(verts)
+        all_faces.append(faces + face_offset)
+        color_value = i + 1
+        all_values.extend([color_value] * len(verts))
+        face_offset += len(verts)
+    combined_verts = np.vstack(all_verts)
+    combined_faces = np.vstack(all_faces)
+    combined_values = np.array(all_values)
+    return combined_verts, combined_faces, combined_values, custom_colormap
+
+# Image loader widget
+@magicgui(call_button='Load', imagefile={'widget_type': 'FileEdit', 'label': 'Image'},
+          zratio={'widget_type': 'FloatSlider', 'min': 1, 'max': 9})
+def load_image_tiff(vw:Viewer, imagefile=imagefile_default, zratio=6):
+
+    # Close all layers
+    vw.layers.clear()
+
+    # Load image, split channels and display in viewer
+    img = imread(imagefile).astype(np.uint16)
+    nuclei = img[:, 0, :, :]
+    membrane = img[:, 1, :, :]
+    vw.add_image(nuclei, name=f"Nuclei", scale=(zratio, 1, 1))
+    vw.add_image(membrane, name=f"Membrane", scale=(zratio, 1, 1), blending='additive', colormap='green')
+
+    return None
